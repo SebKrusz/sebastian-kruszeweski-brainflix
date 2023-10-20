00cde5da7a670048fac73a82419b1e8a246020b0
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2023-10-20 10:54:01 -0700

    comment section functional styles 70%

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.scss';
 import Header from './components/header/header.js';
 import Video from './components/video/video.js';
 import Videodescription from './components/video-description/video-description.js';
+import Commentsection from './components/comment-section/comment-section';
+import videoDetails from './data/video-details.json';
+
 
 function App() {
   const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
@@ -15,7 +18,7 @@ function App() {
       <Header />
       <Video posterImage={selectedVideo.image} />
       <Videodescription selectedVideo={selectedVideo}/>
-
+      <Commentsection videoData={selectedVideo} />
       
     </>
   );
---
+++ src/components/comment-section/comment-section.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './comment-section.scss';
+import yourimg from '../../assets/images/images/Mohan-muruge.jpg';
+
+function Commentsection({ videoData }) {
+  const [comments, setComments] = useState(videoData.comments || []);
+  const [newComment, setNewComment] = useState('');
+
+  const handleCommentChange = (e) => {
+    setNewComment(e.target.value);
+  };
+
+  const handleSubmitComment = (e) => {
+    e.preventDefault();
+
+    const newCommentObj = {
+      id: comments.length + 1,
+      name: 'Your Name',
+      text: newComment,
+      timestamp: new Date().toLocaleString(),
+    };
+
+    setComments([...comments, newCommentObj]);
+    setNewComment('');
+  };
+
+  return (
+    <div className="comment-section">
+      <h2 className="comment-section__header">Join The Conversation</h2>
+      <form className="comment-section__my-form" onSubmit={handleSubmitComment}>
+        <img className="comment-section__your-img" src={yourimg} alt="User Image" />
+        <div className="comment-section__user-comment">
+          <textarea
+            type="text"
+            name="comment"
+            value={newComment}
+            onChange={handleCommentChange}
+            className="comment-section__your-comment"
+            id="yourComment"
+            placeholder="Add a new comment"
+          />
+        </div>
+        <div className="comment-section__button">
+          <button type="submit" className="comment-section__button--clickable" id="add-comment">
+            COMMENT
+          </button>
+        </div>
+      </form>
+      <div className="comments-container">
+        {comments.map((comment) => (
+          <div key={comment.id} className="comments__single-comment">
+            <img className="comments__single-comment--img" src={yourimg} alt="User Image" />
+            <div className="comments__single-comment--name-date-container">
+              <p className="comments__single-comment--name">{comment.name}</p>
+              <p className="comments__single-comment--date">{comment.timestamp}</p>
+            </div>
+            <p className="comments__single-comment--text">{comment.text || comment.comment}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Commentsection;
